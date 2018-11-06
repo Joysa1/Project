4b958f3f6c2fb851bf51caa906a54b0454280b88
Stennik Nikita <nikita-stennik@mail.ru>
2018-11-06 10:25:19 +0300

    Spring JPA

---
+++ CoursesSpringJPA/src/main/java/coursesspringjpa/springjpa/User.java
@@ -0,0 +1,136 @@
+package coursesspringjpa.springjpa;
+
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table (name="users")
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idusers;
+    private String username;
+    private  String password;
+    private  boolean admin;
+    private  String name;
+    private  String surname;
+    private  String lastname;
+    private  int age;
+    private String number;
+
+    public User() {
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+       this.number = number;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public boolean isAdmin() {
+        return admin;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (idusers != user.idusers) return false;
+        if (admin != user.admin) return false;
+        if (age != user.age) return false;
+        if (!username.equals(user.username)) return false;
+        if (!password.equals(user.password)) return false;
+        if (!name.equals(user.name)) return false;
+        if (!surname.equals(user.surname)) return false;
+        if (!lastname.equals(user.lastname)) return false;
+        return number.equals(user.number);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = idusers;
+        result = 31 * result + (username != null ? username.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        result = 31 * result + (admin ? 1 : 0);
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (surname != null ? surname.hashCode() : 0);
+        result = 31 * result + (lastname != null ? lastname.hashCode() : 0);
+        result = 31 * result + age;
+        result = 31 * result + (number != null ? number.hashCode() : 0);
+        return result;
+    }
+
+
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", admin=" + admin +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", age=" + age +
+                ", number='" + number + '\'' +
+                '}';
+    }
+}
